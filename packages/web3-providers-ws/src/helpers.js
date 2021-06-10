8bd8e005e97b3187d96d6d6384b7863575aff97e
Maxime <maxime@vaugam.com>
2021-06-10 14:34:42 +0200

    fix(wechat): fix btoa for wechat env

--- packages/web3-providers-ws/src/helpers.js
+++ packages/web3-providers-ws/src/helpers.js
@@ -19,7 +19,13 @@ if (isNode || isRN) {
         helpers = require('url').parse;
     }
 } else {
-    _btoa = btoa.bind(window);
+    _btoa = function(a){
+        var c, d, e, f, g, h, i, j, o, b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", k = 0, l = 0, m = "", n = [];
+        if (!a) return a;
+        do c = a.charCodeAt(k++), d = a.charCodeAt(k++), e = a.charCodeAt(k++), j = c << 16 | d << 8 | e,
+            f = 63 & j >> 18, g = 63 & j >> 12, h = 63 & j >> 6, i = 63 & j, n[l++] = b.charAt(f) + b.charAt(g) + b.charAt(h) + b.charAt(i); while (k < a.length);
+        return m = n.join(""), o = a.length % 3, (o ? m.slice(0, o - 3) :m) + "===".slice(o || 3);
+    }
     helpers = function(url) {
         return new URL(url);
     };
